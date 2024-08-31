ba8445a18634c7a3b70ccd61dabbe11809067b95
kishorknight <kishorknight103@gmail.com>
2024-08-31 23:01:13 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Ecommerce Shoe Project
+
+This project is an ecommerce platform for buying and selling shoes, built using Node.js and MySQL.
+
+## Features
+
+- User Authentication: Users can sign up, log in, and log out securely.
+- Product Listing: Display available shoes for purchase.
+- Shopping Cart: Users can add shoes to their cart and proceed to checkout.
+- Order Management: Users can view their order history.
+- Admin Panel: Admins can manage products, users, and orders.
+
+## Technologies Used
+
+- Node.js: Backend server environment.
+- Express.js: Web application framework for Node.js.
+- MySQL: Database management system for storing product and user data.
+- React: Frontend framework for responsive design.
+- Bcrypt: Library for hashing passwords.
+- Multer: Middleware for handling multipart/form-data (file uploads).
+- Express-session: Middleware for managing session data.
---
+++ client/src/components/landing/LandingImages.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const LandingImages = () => {
+  return (
+    <section className="section insta-post">
+
+    <ul className="insta-post-list has-scrollbar flex w-full justify-evenly">
+    
+      <li className="insta-post-item">
+        <img src="src\components\landing\images\insta-1.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+        <img src="src\components\landing\images\insta-2.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+      <img src="src\components\landing\images\insta-3.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+      <img src="src\components\landing\images\insta-4.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+      <img src="src\components\landing\images\insta-5.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+      <img src="src\components\landing\images\insta-6.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+      <img src="src\components\landing\images\insta-7.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+    
+      </li>
+    
+      <li className="insta-post-item">
+      <img src="src\components\landing\images\insta-8.jpg" width="190.08" height="190.08" loading="lazy" alt="Instagram post"
+          className="insta-post-banner image-contain"/>
+    
+      </li>
+    
+    </ul>
+    
+        </section>
+  )
+}
+
+export default LandingImages
